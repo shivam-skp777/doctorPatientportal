b28ac41920472d65003357f413adea1d05744643
lapitsgovind <govind@lapits.com>
2020-04-29 09:29:49 +0530

    build

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+// app.use(express.static('./dist/Admin'));
+app.use(express.static(__dirname + '/dist/doctor-patient'));
+
+// app.use(express.static(__dirname/front-end/dist/));
+
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/doctor-patient/index.html'));
+});
+
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
