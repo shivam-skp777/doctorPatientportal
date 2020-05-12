cfc7359f7a62d2ff8ecabaa246897ffab0e183e4
Shivam Kumar Pandey <Shivam.sp009777@gmail.com>
2020-05-12 22:29:07 +0530

    Button

--- src/app/components/complete-report/complete-report.component.ts
+++ src/app/components/complete-report/complete-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AfterLoginService } from 'src/app/provider/after-login.service';
 import { AdminService } from 'src/app/provider/admin.service';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-complete-report',
@@ -9,7 +10,7 @@ import { AdminService } from 'src/app/provider/admin.service';
 })
 export class CompleteReportComponent implements OnInit {
   completeReportList = new Array();
-  constructor(public afterLogin: AfterLoginService,public adminService: AdminService) { }
+  constructor(public afterLogin: AfterLoginService,public adminService: AdminService,public route: Router) { }
 
   ngOnInit() {
     this.getCompleteReportList();
@@ -31,5 +32,15 @@ export class CompleteReportComponent implements OnInit {
       this.adminService.showError(err['message'],'Complete Report');
     })
   }
-
+  
+  // View Analysed Report
+  viewReport(reportId){
+    console.log('Report Id--->',reportId)
+    let reportData: NavigationExtras = {
+      queryParams: {
+          reportID: reportId
+      }
+   };
+   this.route.navigate(['/paticular-report'],reportData);
+  }
 }
